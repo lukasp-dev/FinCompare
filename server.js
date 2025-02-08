c837ea43493481e61dff559a788ed752fbc79dc0
lukasp-dev <nubddak3@gmail.com>
2025-02-08 01:04:23 -0500

    FIRST commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-require("dotenv").config(); 
-const mongoose = require("mongoose");
+import dotenv from "dotenv"; 
+import mongoose from "mongoose";
 
+dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB Atlas"))
